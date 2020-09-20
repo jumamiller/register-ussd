1d1ecb9e2abe35c4b1cc30e4adfe688a8b0d3424
Miller Juma <miller.developer.william@gmail.com>
2020-09-20 18:18:47 +0300

    registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,62 +36,63 @@ class UserController extends Controller
         if(!empty($new_level)){
             $this->level=$new_level;
         }
-        switch ($this->level){
-            case 0:
-                switch ($this->user_response){
-                    case "":
-                        Session::create([
-                            'session_id'    =>$this->session_id,
-                            'service_code'  =>$this->service_code,
-                            'phone_number'  =>$this->phone_number,
-                            'session_level' =>1
-                        ]);
-                        User::create([
-                            'phone_number'  =>$this->phone_number
-                        ]);
-                        $this->screen_response="Register to access our services\n";
-                        $this->screen_response.="1:Register\n";
-                        $this->screen_response.="2:Login";
 
-                        $this->ussd_proceed($this->screen_response);
-                        break;
-                }
-                break;
-            case 1:
-                switch($this->user_response){
-                    case "1":
-                        $this->screen_response.="Enter your first name\n";
-                        $this->ussd_proceed($this->screen_response);
-                        (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>2]);
-                        break;
-                }
-                break;
-            case 2:
-                (new User())->where("phone_number",$this->phone_number)->update(["first_name"=>$this->user_response]);
-                $this->screen_response="Enter your last name\n";
-                $this->ussd_proceed($this->screen_response);
-                (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>3]);
-                break;
-            case 3:
-                (new User())->where("phone_number",$this->phone_number)->update(["last_name"=>$this->user_response]);
-                $this->screen_response="Enter your email address\n";
-                $this->ussd_proceed($this->screen_response);
-                (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>4]);
-                break;
-            case 4:
-                (new User())->where("phone_number",$this->phone_number)->update(["email"=>$this->user_response]);
-                $this->screen_response="Enter your password\n";
-                $this->ussd_proceed($this->screen_response);
-                (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>5]);
-                break;
-            case 5:
-                (new User())->where("phone_number",$this->phone_number)->update(["password"=>Hash::make($this->user_response)]);
-                $user=(new user())->where('phone_number',$this->phone_number)->pluck('last_name')->first();
+            switch ($this->level){
+                case 0:
+                    switch ($this->user_response){
+                        case "":
+                            Session::create([
+                                'session_id'    =>$this->session_id,
+                                'service_code'  =>$this->service_code,
+                                'phone_number'  =>$this->phone_number,
+                                'session_level' =>1
+                            ]);
+                            User::create([
+                                'phone_number'  =>$this->phone_number
+                            ]);
+                            $this->screen_response="Register to access our services\n";
+                            $this->screen_response.="1:Register\n";
+                            $this->screen_response.="2:Login";
 
-                $this->screen_response=
-                    "<strong>Congratulations <i>$user</i></strong>,you have successfully registered on Bikebuka Online Classes\n";
-                $this->ussd_finish($this->screen_response);
-                break;
+                            $this->ussd_proceed($this->screen_response);
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch($this->user_response){
+                        case "1":
+                            $this->screen_response.="Enter your first name\n";
+                            $this->ussd_proceed($this->screen_response);
+                            (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>2]);
+                            break;
+                    }
+                    break;
+                case 2:
+                    (new User())->where("phone_number",$this->phone_number)->update(["first_name"=>$this->user_response]);
+                    $this->screen_response="Enter your last name\n";
+                    $this->ussd_proceed($this->screen_response);
+                    (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>3]);
+                    break;
+                case 3:
+                    (new User())->where("phone_number",$this->phone_number)->update(["last_name"=>$this->user_response]);
+                    $this->screen_response="Enter your email address\n";
+                    $this->ussd_proceed($this->screen_response);
+                    (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>4]);
+                    break;
+                case 4:
+                    (new User())->where("phone_number",$this->phone_number)->update(["email"=>$this->user_response]);
+                    $this->screen_response="Enter your password\n";
+                    $this->ussd_proceed($this->screen_response);
+                    (new Session())->where("phone_number",$this->phone_number)->update(['session_level'=>5]);
+                    break;
+                case 5:
+                    (new User())->where("phone_number",$this->phone_number)->update(["password"=>Hash::make($this->user_response)]);
+                    $user=(new user())->where('phone_number',$this->phone_number)->pluck('first_name')->first();
+
+                    $this->screen_response=
+                        "Congratulations <strong><i>$user</i></strong>,you have successfully registered for JKUAT Online Classes\n";
+                    $this->ussd_finish($this->screen_response);
+                    break;
         }
     }
     public function ussd_proceed($proceed)
